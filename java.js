7c23c7a87efdcd7f4930fccf44d87ea43c09514f
FabianAntal-Git <herr.antal.fabian@gmail.com>
2026-05-22 15:30:52 +0200

    Neuster Stand

--- java.js
+++ java.js
@@ -1,1 +1,17 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', function(){
+	const burger = document.getElementById('burger');
+	const nav = document.getElementById('main-nav');
+
+	function toggleMenu(){
+		const open = nav.classList.toggle('open');
+		burger.setAttribute('aria-expanded', open);
+		nav.setAttribute('aria-hidden', !open);
+	}
+
+	burger.addEventListener('click', toggleMenu);
+
+
+	
+});
+
+
