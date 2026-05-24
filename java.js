e17b16b727fea390343db4869b7d605d68a53644
FabianAntal-Git <herr.antal.fabian@gmail.com>
2026-05-25 00:51:45 +0200

    Bilder eingefügt, Header angepasst, Hover Effekt hinzugefügt, Logo positioniert

--- java.js
+++ java.js
@@ -1,16 +1,18 @@
 ﻿document.addEventListener('DOMContentLoaded', function(){
-	const burger = document.getElementById('burger');
-	const nav = document.getElementById('main-nav');
+	const burger = document.getElementById('NaviBut');
+	const nav = document.getElementById('Navi');
+	
+	
 
-	function toggleMenu(){
+
+	function toggleMenu()
+	{
 		const open = nav.classList.toggle('open');
-		burger.setAttribute('aria-expanded', open);
+		nav.setAttribute('aria-expanded', open);
 		nav.setAttribute('aria-hidden', !open);
 	}
 
 	burger.addEventListener('click', toggleMenu);
-
-
 	
 });
 
