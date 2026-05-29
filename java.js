297ba775e2f1bbcb1cb5386e457caf9a666ddc61
FabianAntal-Git <herr.antal.fabian@gmail.com>
2026-05-29 09:13:30 +0200

    Rework showSlides and pushed into contentLoaded

--- java.js
+++ java.js
@@ -1,7 +1,9 @@
-﻿document.addEventListener('DOMContentLoaded', function(){
+﻿// Wait for Document to Load
+document.addEventListener('DOMContentLoaded', function(){
 	const burger = document.getElementById('NaviBut');
 	const nav = document.getElementById('Navi');
 	
+  //toggleMenueFunction
 	function toggleMenu()
 	{
 		const open = nav.classList.toggle('open');
@@ -9,11 +11,41 @@
 		nav.setAttribute('aria-hidden', !open);
 	}
 
+  //Burger click event
 	burger.addEventListener('click', toggleMenu);
 	
+const buttons = document.querySelectorAll('.dot');
+let slides = document.getElementsByClassName("mySlides");
+
+//Show Slides on Button click event
+function showSlides(){
+buttons.forEach((button, indexButton) => {
+  
+  button.addEventListener('click', () => {
+     
+    // Remove "active" class from all buttons
+    buttons.forEach(btn => btn.classList.remove('active'));
+    
+    // Add "active" class to the clicked button
+    button.classList.add('active');
+    slides[indexButton].style.display = "flex";
+
+    // Hide all other slides
+    buttons.forEach((btn, i) => {
+      if (i !== indexButton) {
+        slides[i].style.display = "none";
+      }
+    });
+    // Log the index of the clicked button an active state
+      console.log(`${indexButton}`+' and '+`${button.classList.contains('active')}`);
+    }); 
+  });
+}
+// call ShowSlides
+showSlides(); 
 });
 
-const slidesQ = document.querySelectorAll('.mySlides');
+/*
 const buttons = document.querySelectorAll('.dot');
 let slides = document.getElementsByClassName("mySlides");
 
@@ -35,17 +67,19 @@ buttons.forEach((button, indexButton) => {
         slides[i].style.display = "none";
       }
     });
-    // Log the index of the clicked button
-    console.log(`${SlideIndex}`);
+    // Log the index of the clicked button an active state
+      console.log(`${indexButton}`+' and '+`${button.classList.contains('active')}`);
     }); 
   });
+
+  
 }
 
 
 
 ShowSlides();
 
-
+*/
 /*
 showSlides(slideIndex);
 
